68823305beeacfa64d94c1ddb1f4ee32f50f1878
eatlunchnow <eatlunchnow@gmail.com>
2019-02-19 23:33:54 -0800

    Exercise 4

---
+++ program4.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+var filePath = process.argv[2];
+
+fs.readFile(filePath, function callback(err, data){
+	if (err){return err}
+	var str = data.toString();
+	console.log(str.split('\n').length - 1);
+})
+
+
+
+
+
