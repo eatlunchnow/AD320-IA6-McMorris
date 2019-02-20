dadd923073727ee2637b3064a7348ba575d614b7
eatlunchnow <eatlunchnow@gmail.com>
2019-02-19 22:25:01 -0800

    Exercise 2

--- program2.js
+++ program2.js
@@ -1,1 +1,12 @@
-console.log(process.argv)
+/*console.log(process.argv)*/
+
+var el = process.argv.length;
+var sum = 0;
+
+for(var i = 2; i < el; i++){
+	var str = process.argv[i];
+	var num = Number(str);
+	sum = sum + num;
+}
+
+console.log(sum);
